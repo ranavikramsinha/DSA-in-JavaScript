d72e5d286eb70bd337222be6278e4c54e65908dd
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-06-05 15:04:57 +0530

    stack

---
+++ 01_Stack/stack.js
@@ -0,0 +1,59 @@
+class Stack{
+    constructor(){
+        this.stack = []
+    }
+
+    //* stack.push()
+    push(data){
+        this.stack.push(data)
+    }
+
+    pop(){
+        this.stack.pop()
+    }
+
+    peek(){
+        return this.stack[this.stack.length - 1]
+    }
+
+    isEmpty(){
+        return this.stack.length === 0
+    }
+
+    size(){
+        return this.stack.length
+    }
+
+    clear(){
+        this.stack = []
+    }
+
+    contains(element){
+        return this.stack.includes(element)
+    }
+
+    reverse(){
+        this.stack.reverse()
+    }
+
+    printStack(){
+        let str = ""
+
+        for(let i = 0; i < this.stack.length; i++){
+            str += this.stack[i] + "\n"
+        }
+
+        return str
+    }
+}
+
+//* Usage Example
+
+let myStack = new Stack()
+
+myStack.push(11)
+myStack.push(55)
+myStack.push(33)
+myStack.pop()
+console.log(myStack.printStack())
+console.log(myStack)
