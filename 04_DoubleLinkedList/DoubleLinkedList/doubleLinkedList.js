b0d9826609752a727006bdef036a157eb8fef307
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-06-07 19:53:23 +0530

    doublyLinkedList insert at the beginning

---
+++ 04_DoubleLinkedList/DoubleLinkedList/doubleLinkedList.js
@@ -0,0 +1,38 @@
+class Node{
+    constructor(data, next = null, prev = null){
+        this.data = data
+        this.next = next
+        this.prev = prev
+    }
+}
+
+class doublyLinkedList{
+    constructor(){
+        this.head = null
+        this.tail = null
+    }
+}
+
+doublyLinkedList. prototype.insertAtBeginning = function(data){
+    const newNode = new Node (data, this.head, null)
+
+    if(this.head !== null){
+        this.head.prev = newNode
+    }
+
+    this.head = newNode
+    
+    if(this.tail === null){
+        this.tail = newNode
+    }
+}
+
+const list = new doublyLinkedList()
+list.insertAtBeginning(10)
+list.insertAtBeginning(20)
+list.insertAtBeginning(30)
+
+// console.log(list.head.data) // 30
+// console.log(list.tail.data) // 10
+// console.log(list.head.next.data) // 20
+// console.log(list.tail.prev.data) // 20
