e13813f00134e6e2f7c68c45eb53b51499e842d3
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-06-08 23:02:05 +0530

    doubly linked list insert at last

--- 04_DoubleLinkedList/DoubleLinkedList/doubleLinkedList.js
+++ 04_DoubleLinkedList/DoubleLinkedList/doubleLinkedList.js
@@ -27,6 +27,21 @@ doublyLinkedList. prototype.insertAtBeginning = function(data){
     }
 }
 
+doublyLinkedList.prototype.insertAtLast = function(data){
+    const newNode = new Node(data, null, this.tail)
+
+    if(this.tail !== null){
+        this.tail.next = newNode
+    }
+
+    this.tail = newNode
+
+    if(this.head === null){
+        this.head = newNode
+    }
+
+}
+
 const list = new doublyLinkedList()
 list.insertAtBeginning(10)
 list.insertAtBeginning(20)
@@ -36,3 +51,12 @@ list.insertAtBeginning(30)
 // console.log(list.tail.data) // 10
 // console.log(list.head.next.data) // 20
 // console.log(list.tail.prev.data) // 20
+
+list.insertAtLast(40)
+list.insertAtLast(50)
+list.insertAtLast(60)
+
+// console.log(list.head.data) // 30
+// console.log(list.tail.data) // 60
+// console.log(list.head.next.data) // 20
+// console.log(list.tail.prev.data) // 50
