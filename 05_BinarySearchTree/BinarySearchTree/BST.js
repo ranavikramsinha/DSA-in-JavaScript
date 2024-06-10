4f7a628f14ce038f935830279277ae2abb5eaca9
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-06-10 17:01:28 +0530

    inOrder, PreOrder and postOrder in BST

--- 05_BinarySearchTree/BinarySearchTree/BST.js
+++ 05_BinarySearchTree/BinarySearchTree/BST.js
@@ -41,65 +41,17 @@ class binarySearchTree{
         }
     }
 
-    // delete(key){
-    //     this.root = this.deleteNode(this.root, key)
-    // }
-
-    // deleteNode(node, key){ //* node --> root
-    //     if(node === null){
-    //         return null
-    //     }
-
-    //     if(key < node.key){
-    //         node.left = this.deleteNode(node.left, key)
-    //     }
-
-    //     else if(key > node.key){
-    //         node.right = this.deleteNode(node.right, key)
-    //     }
-
-    //     else{
-    //         if(node.left === null && node.right === null){
-    //             return null
-    //         }
-
-    //         else if(node.left === null){
-    //             return node.right
-    //         }
-
-    //         else if(node.right === null){
-    //             return node.left
-    //         }
-
-    //         else{
-    //             let tempNode = this.findMinNode(node.right)
-    //             node.key = tempNode.key
-    //             node.right = this.deleteNode(node.right, tempNode.key)
-    //         }
-    //     }
-
-    //     return node
-    // }
-
-    // findMinNode(node){
-    //     while(node.left !== null){
-    //         node = node.left
-    //     }
-
-    //     return node
-    // }
-
     delete(key){
         this.root = this.deleteNode(this.root, key)
     }
 
-    deleteNode(node, key){  // node --> root
+    deleteNode(node, key){ //* node --> root
         if(node === null){
             return null
         }
 
         if(key < node.key){
-            node.left = this.delete(node.left, key)
+            node.left = this.deleteNode(node.left, key)
         }
 
         else if(key > node.key){
@@ -120,22 +72,70 @@ class binarySearchTree{
             }
 
             else{
-                let tempNode = this.findMaxNode(node.left)
+                let tempNode = this.findMinNode(node.right)
                 node.key = tempNode.key
-                node.left = this.deleteNode(node.left, tempNode)
+                node.right = this.deleteNode(node.right, tempNode.key)
             }
         }
 
         return node
     }
 
-    findMaxNode(node){
-        while(node.right !== null){
-            node = node.right
+    findMinNode(node){
+        while(node.left !== null){
+            node = node.left
         }
 
         return node
     }
+
+    // delete(key){
+    //     this.root = this.deleteNode(this.root, key)
+    // }
+
+    // deleteNode(node, key){  // node --> root
+    //     if(node === null){
+    //         return null
+    //     }
+
+    //     if(key < node.key){
+    //         node.left = this.delete(node.left, key)
+    //     }
+
+    //     else if(key > node.key){
+    //         node.right = this.deleteNode(node.right, key)
+    //     }
+
+    //     else{
+    //         if(node.left === null && node.right === null){
+    //             return null
+    //         }
+
+    //         else if(node.left === null){
+    //             return node.right
+    //         }
+
+    //         else if(node.right === null){
+    //             return node.left
+    //         }
+
+    //         else{
+    //             let tempNode = this.findMaxNode(node.left)
+    //             node.key = tempNode.key
+    //             node.left = this.deleteNode(node.left, tempNode)
+    //         }
+    //     }
+
+    //     return node
+    // }
+
+    // findMaxNode(node){
+    //     while(node.right !== null){
+    //         node = node.right
+    //     }
+
+    //     return node
+    // }
 
     inorderTraversal(){
         const result = []
@@ -153,7 +153,7 @@ class binarySearchTree{
 
     preOrderTraversal(){
         const result = []
-        this.preOrderTraversal(this.root, result)
+        this.preOrder(this.root, result)
         return result
     }
 
@@ -180,3 +180,21 @@ class binarySearchTree{
     }
 
 }
+
+let bst = new binarySearchTree()
+
+bst.insert(50)
+bst.insert(30)
+bst.insert(70)
+bst.insert(20)
+bst.insert(40)
+bst.insert(60)
+bst.insert(80)
+
+bst.delete(20)
+bst.delete(30)
+bst.delete(50)
+
+console.log("In-order Traversal:", bst.inorderTraversal())
+console.log("Pre-order Traversal:", bst.preOrderTraversal())
+console.log("Post-order Traversal:", bst.postOrderTraversal())
