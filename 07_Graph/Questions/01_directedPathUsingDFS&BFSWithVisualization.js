f8fa4c5c9b9d63cfc000ad2906092eb21abd42dd
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-06-17 20:25:38 +0530

    directed path - DFS & BFS

---
+++ 07_Graph/Questions/01_directedPathUsingDFS&BFSWithVisualization.js
@@ -0,0 +1,90 @@
+/*
+Given two nodes (start and dest) in a directed acyclic graph (DAG), return true if there is a directed path from start to dest, otherwise return false.
+
+Example 1:
+
+Input: start = 1, dest = 3
+Output: true
+Explanation: There is a directed path 1 -> 2 -> 3.
+Example 2:
+
+Input: start = 1, dest = 4
+Output: false
+Explanation: There is no path from 1 to 4.
+
+*/
+
+const graph = {
+    a : ['b', 'c'],
+    b: ['d', 'e'],
+    c: ['f'],
+    d: [],
+    e: ['f'],
+    f: []
+}
+
+
+function hasPathDFS(graph, start, dest){
+    const stack = [start]
+    const visited = new Set()
+    console.log(`Starting DFS from ${start} to ${dest}`)
+
+    while(stack.length > 0){
+        const node = stack.pop()
+        console.log(`Popped ${node} from stack`)
+
+        if(node === dest){
+            console.log(`Found destination ${dest}`)
+            return true
+        }
+
+        if(!visited.has(node)){
+            visited.add(node)
+            console.log(`Visited ${node}`)
+
+            for(const neighbour of graph[node]){
+                console.log(`Adding neighbour ${neighbour} of ${node} to stack`)
+                stack.push(neighbour)
+            }
+        }
+    }
+
+    console.log(`No path found from ${start} to ${dest}`)
+    return false
+}
+
+
+function hasPathBFS(graph, start, dest){
+    const queue = [start]
+    const visited = new Set()
+    console.log(`Starting BFS from ${start} to ${dest}`)
+
+    while(queue.length > 0){
+        const node = queue.shift()
+        console.log(`Dequeued ${node} from queue`)
+
+        if(node === dest){
+            console.log(`Found destination ${dest}`)
+            return true
+        }
+
+        if(!visited.has(node)){
+            visited.add(node)
+            console.log(`Visited ${node}`)
+
+            for(const neighbour of graph[node]){
+                console.log(`Adding neighbour ${neighbour} of ${node} to queue`)
+                queue.push(neighbour)
+            }
+        }
+    }
+
+    console.log(`No path found from ${start} to ${dest}`)
+    return false
+}
+
+
+console.log(hasPathDFS(graph, 'a', 'f')) //* true
+console.log(hasPathDFS(graph, 'd', 'e')) //* false
+console.log(hasPathBFS(graph, 'a', 'f')) //* true
+console.log(hasPathBFS(graph, 'd', 'e')) //* false
