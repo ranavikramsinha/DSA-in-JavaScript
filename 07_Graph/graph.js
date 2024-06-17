6f5e430dd67c8053bda7eb73ac83ac97209cce0a
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-06-17 12:49:10 +0530

    print visited in DFS of graph

--- 07_Graph/graph.js
+++ 07_Graph/graph.js
@@ -26,9 +26,14 @@ function depthFirstSearch(graph, start){
             }
         }
     }
+
+    return visited
 }
 
 depthFirstSearch(graph, 'a') //* The DFS algorithm visits nodes in the order of a, c, d, b starting from node a
+
+const visitedNodes = depthFirstSearch(graph, 'a')
+console.log(visitedNodes) //* Set(4) { 'a', 'c', 'd', 'b' }
 
 console.log("************************************")
 
@@ -45,6 +50,11 @@ function depthFirstSearchRecursive(graph, start, visited = new Set()){
         }
     }
 
+    return visited
+
 }
 
 depthFirstSearchRecursive(graph, 'a')
+
+const visitedNodesRecursive = depthFirstSearchRecursive(graph, 'a')
+console.log(visitedNodesRecursive) //* Set(4) { 'a', 'b', 'c', 'd' }
