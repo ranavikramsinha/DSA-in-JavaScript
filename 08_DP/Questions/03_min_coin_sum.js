e9aee6f18b1ec29afbdd4ee0e712c680af5c2303
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-06-24 22:34:22 +0530

    minimum and maximum number of coins to get the sum(amount) in DP (memoization & tabulation)

--- 08_DP/Questions/03_min_coin_sum.js
+++ 08_DP/Questions/03_min_coin_sum.js
@@ -37,7 +37,7 @@ function minCoinTab(amount, coins){
 
     if(amount < 0) return -1
 
-    let table = Array(n+1).fill(Infinity)
+    let table = Array(amount + 1).fill(Infinity)
 
     table[0] = 0
 
@@ -51,4 +51,59 @@ function minCoinTab(amount, coins){
     
 }
 
-console.log(minCoin(10, [1,2,5], memo = {})) //* 2
+console.log(minCoinTab(10, [1,2,5], memo = {})) //* 2
+
+//* Memoization
+function maxCoin(amount, coins, memo = {}){
+    if(amount in memo) {
+        return memo[amount]
+    }
+
+    if(amount === 0){
+        return 0
+    }
+
+    if(amount < 0){
+        return -1
+    }
+
+    let max = 0
+
+    for(const coin of coins){
+        const result = maxCoin(amount-coin, coins, memo)
+
+        if(result !== -1){
+            max = Math.max(max, result + 1)
+        }
+    }
+
+    memo[amount] = max
+
+    return memo[amount]
+}
+
+console.log(maxCoin(11, [1,2,5], {})) //* 11
+
+
+//* Tabulation
+function maxCoinTab(amount, coins){
+
+    if(amount === 0) return 0
+
+    if(amount < 0) return -1
+
+    let table = Array(amount + 1).fill(0)
+
+    table[0] = 0
+
+    for(const coin of coins){
+        for(let i = coin; i <= amount; i++){
+            table[i] = Math.max(table[i], table[i - coin] + 1)
+        }
+    }
+
+    return table[amount]
+    
+}
+
+console.log(maxCoinTab(10, [1,2,5], {})) //* 10
