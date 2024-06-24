950ef62e2202e53e6523b279ee0d51ffce339b27
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-06-24 22:22:39 +0530

    unique paths in DP (memoization) with diagram for visualization

--- 08_DP/Questions/05_unique_paths.js
+++ 08_DP/Questions/05_unique_paths.js
@@ -21,5 +21,22 @@ function uniquePaths(m, n, memo = {}) {
     
 }
 
-console.log(uniquePaths(3, 3, memo = {}))
-console.log(uniquePaths(3, 7, memo = {}))
+console.log(uniquePaths(3, 3, {}))
+console.log(uniquePaths(3, 7, {}))
+
+//* Start (3,3)
+//* |
+//* +-- (2,3)    +-- (3,2)
+//* |            |
+//* |            |
+//* +-- (1,3)    +-- (2,2)    +-- (3,1)
+//* |            |            |
+//* |            |            |
+//* +-- (0,3)    +-- (1,2)    +-- (2,1)    +-- (3,0)
+//*              |            |            |
+//*              +-- (0,2)    +-- (1,1)    +-- (2,0)
+//*                           |            |
+//*                           +-- (0,1)    +-- (1,0)
+//*                                        |
+//*                                        +-- (0,0)
+//* 
