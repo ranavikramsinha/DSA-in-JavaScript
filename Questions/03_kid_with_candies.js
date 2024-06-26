2461239c80df053612fa19e37242f8fccbc165d3
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-06-26 23:28:42 +0530

    add numbers to file and kid with candies problem

---
+++ Questions/03_kid_with_candies.js
@@ -0,0 +1,9 @@
+https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/description/
+
+var kidsWithCandies = function(candies, extraCandies) {
+
+    const maxCandy = Math.max(...candies) //* // Find the maximum number of candies any kid currently has
+
+    return candies.map(candy => candy + extraCandies >= maxCandy) //* Check if each kid can have the greatest number of candies with extraCandies
+    
+};
