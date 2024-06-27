8d0f1802a961dba7bab001ac2726a64374a9c90e
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-06-27 23:29:00 +0530

    reverse words in a string problem

---
+++ Questions/06_reverse_words_in_a_string.js
@@ -0,0 +1,47 @@
+//* https://leetcode.com/problems/reverse-words-in-a-string/
+
+// var reverseWords = function (s) {
+//   let stringIntoArray = s.split(" ").filter((word) => word !== "");
+
+//   console.log(stringIntoArray);
+
+//   let result = "";
+
+//   for (let i = stringIntoArray.length - 1; i >= 0; i--) {
+//     result += stringIntoArray[i] + " ";
+//   }
+
+//   return result.trim(); //* for last space
+// };
+
+var reverseWords = function (s) {
+  let string = s.split(" ").filter((word) => word !== "").reverse().join(" ");
+  return string;
+};
+
+console.log(reverseWords("  hello world  "));
+
+// var reverseWords = function(s){
+
+//     let result = []
+//     let length = s.length
+
+//     let word = ""
+
+//     for(let i = 0; i < length; i++){
+//         if(s[i] !== " "){
+//             word += s[i]
+//         }
+//         else if(word.length > 0){
+//             result.unshift(word)
+//             word = ""
+//         }
+//     }
+
+//     if(word.length > 0){
+//         result.unshift(word)
+//     }
+
+//     return result.join("")
+
+// }
