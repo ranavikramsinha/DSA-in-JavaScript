acdc28577f8cc0853436b335fc409e47335e3b1a
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-07-02 20:56:55 +0530

    unique number of occurrence

--- Questions/21_unique_number_of_occurrence.js
+++ Questions/21_unique_number_of_occurrence.js
@@ -2,6 +2,22 @@
 
 var uniqueOccurrences = function(arr) {
 
-    
+    const count = new Map()
+
+    for(const num of arr){
+        count.set(num, (count.get(num) || 0) + 1)
+    }
+
+    const set = new Set()
+
+    for(const c of count.values()){
+        if(set.has(c)){
+            return false
+        }
+
+        set.add(c)
+    }
+
+    return true
     
 };
