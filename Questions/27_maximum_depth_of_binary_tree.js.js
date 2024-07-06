0a3b9fc80616485afbc71654d6b6e5037c730c86
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-07-06 22:37:14 +0530

    maximum depth of binary tree problem

--- Questions/27_maximum_depth_of_binary_tree.js.js
+++ Questions/27_maximum_depth_of_binary_tree.js.js
@@ -1,14 +1,34 @@
 //* https://leetcode.com/problems/maximum-depth-of-binary-tree/
 
-var maxDepth = function(root) {
+function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val;
+  this.left = left === undefined ? null : left;
+  this.right = right === undefined ? null : right;
+}
 
-    if(!root){
-        return 0
+function buildTreeFromArray(arr, index = 0) {
+    if (index >= arr.length || arr[index] === null) {
+        return null;
     }
 
-    const leftDepth = maxDepth(root.left)
-    const rightDepth = maxDepth(root.right)
+    const node = new TreeNode(arr[index]);
+    node.left = buildTreeFromArray(arr, 2 * index + 1);
+    node.right = buildTreeFromArray(arr, 2 * index + 2);
 
-    return Math.max(leftDepth, rightDepth) + 1
-    
+    return node;
+}
+
+var maxDepth = function (TreeNode) {
+  if (!TreeNode) {
+    return 0;
+  }
+
+  const leftDepth = maxDepth(TreeNode.left);
+  const rightDepth = maxDepth(TreeNode.right);
+
+  return Math.max(leftDepth, rightDepth) + 1;
 };
+
+const arr = [1, 2, 3, 4, 5];
+const root = buildTreeFromArray(arr)
+console.log(maxDepth(root));
