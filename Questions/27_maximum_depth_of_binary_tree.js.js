c57f68711abf1149a505c8c63da4d45494290cf7
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-07-06 22:19:29 +0530

    maximum depth of binary tree problem

---
+++ Questions/27_maximum_depth_of_binary_tree.js.js
@@ -0,0 +1,14 @@
+//* https://leetcode.com/problems/maximum-depth-of-binary-tree/
+
+var maxDepth = function(root) {
+
+    if(!root){
+        return 0
+    }
+
+    const leftDepth = maxDepth(root.left)
+    const rightDepth = maxDepth(root.right)
+
+    return Math.max(leftDepth, rightDepth) + 1
+    
+};
