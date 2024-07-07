c4c8c4816e80c11f1f736f3e6012f16db6616241
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-07-07 21:11:14 +0530

    bubble sorting algorithm

---
+++ 09_Sorting/bubble_sorting.js
@@ -0,0 +1,24 @@
+function bubbleSorting(array){
+
+    let isSwap;
+
+    do{
+
+        isSwap = false
+
+        for(let i = 0; i < array.length - 1; i++){
+            if(array[i] > array[i+1]){
+                const temp = array[i]
+                array[i] = array[i+1]
+                array[i+1] = temp
+                isSwap = true
+            }
+        }
+
+    }while(isSwap)
+
+    return array
+
+}
+
+console.log(bubbleSorting([4, 3, 2, 1]))
