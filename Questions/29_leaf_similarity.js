03907bce85ff6648f453c50899be45718a3418ec
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-07-09 23:26:49 +0530

    leaf similarity problem

---
+++ Questions/29_leaf_similarity.js
@@ -0,0 +1,38 @@
+//* https://leetcode.com/problems/leaf-similar-trees/
+
+function Node(val, left, right) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+}
+
+var leafSimilar = function(root1, root2) {
+    
+    let sequence1 = getLeafSequence(root1, "")
+    let sequence2 = getLeafSequence(root2, "")
+    
+    return sequence1 === sequence2
+
+    function getLeafSequence(root, currentSequence) {
+        if (root === null) {
+            return currentSequence;
+        }
+
+        if (root.left === null && root.right === null) {
+            return currentSequence + "-" + root.val
+        }
+
+        let leftSequence = getLeafSequence(root.left, currentSequence);
+        let rightSequence = getLeafSequence(root.right, leftSequence);
+        
+        return rightSequence;
+    }
+}
+
+const root1 = new Node(1, new Node(2, new Node(4), null), new Node(3, new Node(5), new Node(6)))
+const root2 = new Node(7, new Node(2, new Node(4), null), new Node(3, new Node(5), new Node(6)))
+
+// const root1 = new Node(1, new Node(2, new Node(14), null), new Node(3, new Node(15), new Node(16)))
+// const root2 = new Node(7, new Node(2, new Node(4), null), new Node(3, new Node(5), new Node(6)))
+
+console.log(leafSimilar(root1, root2))
