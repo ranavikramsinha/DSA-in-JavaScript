3c63918beaf1ab40ae2ed219f0983b5b6d506fe7
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-07-12 00:03:20 +0530

    odd even LinkedList problem

---
+++ Questions/33_odd_even_linked_list.js
@@ -0,0 +1,34 @@
+//* https://leetcode.com/problems/odd-even-linked-list/
+
+var oddEvenList = function(head) {
+
+    if(head === null || head.next === null){
+        return head
+    }
+
+    let oddHead = new ListNode(0)
+    let oddTail = oddHead
+    let evenHead = new ListNode(0)
+    let evenTail = evenHead
+    let index = 1
+
+    while(head !== null){
+        if(index % 2 === 0){
+            evenTail.next = head
+            evenTail = evenTail.next
+        }
+        else if(index % 2 !== 0){
+            oddTail.next = head
+            oddTail = oddTail.next
+        }
+
+        head = head.next
+        index++
+    }
+
+        evenTail.next = null
+        oddTail.next = evenHead.next
+
+        return oddHead.next
+    
+};
