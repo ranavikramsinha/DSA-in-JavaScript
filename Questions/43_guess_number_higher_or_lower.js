541632d85d2f4fe8a2f39b1cb4ce2087f2aa1f9d
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-07-20 23:18:52 +0530

    guess number higher or lower problem

---
+++ Questions/43_guess_number_higher_or_lower.js
@@ -0,0 +1,25 @@
+//* https://leetcode.com/problems/guess-number-higher-or-lower/
+
+var guessNumber = function(n) {
+
+    let low = 1
+    let high = n
+
+    while(low <= high){
+        const mid = Math.ceil(low + (high - low)/2)
+        const result = guess(mid)
+
+        if(result === 0){
+            return mid
+        }
+        else if(result === -1){
+            high = mid - 1
+        }
+        else if(result === 1){
+            low = mid + 1
+        }
+    }
+
+    return low
+    
+};
