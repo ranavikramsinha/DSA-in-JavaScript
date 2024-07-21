f6e4a8e55b7cb7dfd48449df88ace06c3f798d0f
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-07-21 23:09:17 +0530

    find peak element problem

---
+++ Questions/45_peak_element.js
@@ -0,0 +1,20 @@
+//* 
+
+var findPeakElement = function(nums) {
+
+    let left = 0
+    let right = nums.length - 1
+
+    while(left < right){
+        mid = Math.floor((right + left)/2)
+        
+        if(nums[mid] > nums[mid + 1]){
+            right = mid
+        }
+        else{
+            left = mid + 1
+        }
+    }
+
+    return left  
+};
