bbc703ce2b88ecddea0216820e4d1e8fc49b9070
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-07-24 00:27:22 +0530

    best time to buy and sell stock with transaction fee problem

---
+++ Questions/51_best_time_to_buy_transaction_fee.js
@@ -0,0 +1,39 @@
+//* https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/description/
+
+var maxProfit = function(prices, fee) {
+
+    const n = prices.length
+    const dp = Array.from({length: n}, () => Array(2).fill(-1))
+
+    return profit(prices, 0, n, true)
+
+    function profit(prices, day, n, buy){
+
+        if(day >= n){
+            return 0
+        }
+
+        if(dp[day][buy ? 1 : 0] !== -1){
+            return dp[day][buy ? 1 : 0]
+        }
+
+        let maxProfit;
+
+        if(buy){
+            const take = profit(prices, day + 1, n, false) - prices[day]
+            const notTake = profit(prices, day + 1, n, true)
+
+            maxProfit = Math.max(take, notTake)
+        }
+        else{
+            const sell = prices[day] + profit(prices, day + 1, n, true) - fee
+            const notSell = profit(prices, day + 1, n, false)
+
+            maxProfit = Math.max(sell, notSell)
+        }
+
+        dp[day][buy ? 1 : 0] = maxProfit
+        return maxProfit
+    }
+    
+};
