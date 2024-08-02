36710620e2616b0389ba5af9e47f2e1d6249a3e4
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-08-02 22:52:38 +0530

    remove duplicates from sorted arrry problem

--- Questions/73_remove_duplicates_from_sorted_array.js
+++ Questions/73_remove_duplicates_from_sorted_array.js
@@ -1,18 +1,40 @@
 //* https://leetcode.com/problems/remove-duplicates-from-sorted-array/
 
+// var removeDuplicates = function(nums) {
+
+//     let count = 0;
+//     let n = nums.length;
+//     const set = new Set();
+
+//     for(let i = 0; i < n; i++){
+//         if(!set.has(nums[i])){
+//             set.add(nums[i]);
+//             nums[count] = nums[i];
+//             count++; 
+//         }
+//     }
+    
+//     return count;
+// };
+
+//* time => 0(n) & space => 0(1)
 var removeDuplicates = function(nums) {
 
-    let count = 0;
     let n = nums.length;
-    const set = new Set();
 
-    for(let i = 0; i < n; i++){
-        if(!set.has(nums[i])){
-            set.add(nums[i]);
+    if(n === 0){
+        return 0;
+    }
+
+    let count = 1;
+
+    for(let i = 1; i < n; i++){
+        if(nums[i] !== nums[i - 1]){
             nums[count] = nums[i];
-            count++; 
+            count++
         }
     }
-    
+
     return count;
+    
 };
