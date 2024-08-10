913b2b53f75d563c98e3f4e438c6a1b6b407aa95
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-08-11 00:05:04 +0530

    regions cut by slashes problem

---
+++ Questions/94_regions_cut_by_slashes.js
@@ -0,0 +1,54 @@
+//* 
+
+var regionsBySlashes = function(grid) {
+    let rows = grid.length;
+    let cols = grid[0].length;
+    let regions = 0;
+    let matrix = Array.from({length: rows * 3}, () => Array(cols * 3).fill(0));
+    // let dirs = [[0, 1], [0, -1], [-1, 0], [1, 0]];
+
+    function dfs(matrix, i, j){
+        if(i < 0 || i >= matrix.length || j < 0 || j >= matrix[0].length || matrix[i][j] === 1){
+            return;
+        }
+
+        matrix[i][j] = 1;
+
+        dfs(matrix, i, j + 1);
+        dfs(matrix, i, j - 1);
+        dfs(matrix, i + 1, j);
+        dfs(matrix, i - 1, j);
+
+        // for(const dir of dirs){
+        //     let newI = i + dir[0];
+        //     let newJ = j + dir[1];
+        //     dfs(matrix, newI, newJ);
+        // }
+    }
+
+    for(let i = 0; i < rows; i++){
+        for(let j = 0; j < cols; j++){
+            if(grid[i][j] === '/'){
+                matrix[i * 3][(j * 3) + 2] = 1;
+                matrix[(i * 3) + 1][(j * 3) + 1] = 1;
+                matrix[(i * 3) + 2][j * 3] = 1;
+            }
+            else if(grid[i][j] === '\\'){
+                matrix[i * 3][j * 3] = 1;
+                matrix[(i * 3) + 1][(j * 3) + 1] = 1;
+                matrix[(i * 3) + 2][(j * 3) + 2] = 1;
+            }
+        }
+    }
+
+    for(let i = 0; i < matrix.length; i++){
+        for(let j = 0; j < matrix[0].length; j++){
+            if(matrix[i][j] === 0){
+                dfs(matrix, i, j);
+                regions += 1;
+            }
+        }
+    }
+
+    return regions;
+};
