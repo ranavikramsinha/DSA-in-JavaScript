c20045346fffd4e61f429703cbddb8555c580145
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-08-13 21:15:58 +0530

    combination sum ii problem

--- Questions/100_combination_sum_ii.js
+++ Questions/100_combination_sum_ii.js
@@ -31,25 +31,3 @@ var combinationSum2 = function(candidates, target) {
 
     return result;
 };
-
-//*
-//* Initial Call:
-//* combinationSum2([1, 2, 5, 6, 7, 10], 8)
-//* 
-//* Tree of Recursive Calls:
-//* - [1] 
-//*   - [1, 2]
-//*     - [1, 2, 5] (target - 1 - 2 - 5 = 0; valid)
-//*     - [1, 2, 6] (target - 1 - 2 - 6 = -1; invalid)
-//*   - [1, 5]
-//*     - [1, 5, 7] (target - 1 - 5 - 7 = -5; invalid)
-//*   - [1, 6]
-//*     - [1, 6, 7] (target - 1 - 6 - 7 = -6; invalid)
-//* - [2]
-//*   - [2, 5]
-//*     - [2, 5, 6] (target - 2 - 5 - 6 = -3; invalid)
-//*   - [2, 6]
-//*     - [2, 6] (target - 2 - 6 = 0; valid)
-//* - [5]
-//*   - [5, 6] (target - 5 - 6 = -3; invalid)
-//* 
