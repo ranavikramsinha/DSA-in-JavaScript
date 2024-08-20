fd8818a63e964814b5c53edac715763eb9074f8d
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-08-20 19:52:01 +0530

    store game iii problem

--- Questions/120_stone_game_iii.js
+++ Questions/120_stone_game_iii.js
@@ -1,5 +1,6 @@
 //* https://leetcode.com/problems/stone-game-iii/
 
+//* Recursion + Memo
 var stoneGameIII = function(stoneValue) {
     let n = stoneValue.length;
     let dp = Array(n + 1).fill(-1);
@@ -27,6 +28,36 @@ var stoneGameIII = function(stoneValue) {
         dp[i] = result;
         return dp[i];
     }
+
+    if(result < 0){
+        return 'Bob';
+    }
+    else if(result > 0){
+        return 'Alice';
+    }
+    else{
+        return 'Tie';
+    }
+};
+
+//* Bottom Up
+var stoneGameIII = function(stoneValue) {
+    let n = stoneValue.length;
+    let dp = new Array(n + 1).fill(0);
+    
+    for(let i = n - 1; i >= 0; i--){
+        dp[i] = stoneValue[i] - dp[i + 1];
+
+        if(i + 2 <= n){
+            dp[i] = Math.max(dp[i], stoneValue[i] + stoneValue[i + 1] - dp[i + 2]);
+        }
+
+        if(i + 3 <= n){
+            dp[i] = Math.max(dp[i], stoneValue[i] + stoneValue[i + 1] + stoneValue[i + 2] - dp[i + 3]);
+        }
+    }
+
+    let result = dp[0];
 
     if(result < 0){
         return 'Bob';
