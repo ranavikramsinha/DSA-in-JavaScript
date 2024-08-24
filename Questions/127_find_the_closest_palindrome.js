97e0b1a49fe056e6d5ec557ee195745e690e92b0
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-08-24 23:00:07 +0530

    find the closest palindrome problem

---
+++ Questions/127_find_the_closest_palindrome.js
@@ -0,0 +1,43 @@
+//* https://leetcode.com/problems/find-the-closest-palindrome/
+
+var nearestPalindromic = function(n) {
+    let length = n.length;
+    let middle = Math.ceil(length / 2);
+    let halfOfN = BigInt(n.slice(0, middle), 10);
+    let results = new Set();
+
+    results.add(makePalindrome(halfOfN, length % 2 === 0));
+    results.add(makePalindrome(halfOfN + 1n, length % 2 === 0));
+    results.add(makePalindrome(halfOfN - 1n, length % 2 === 0));
+    results.add(BigInt(Math.pow(10, length - 1)) - 1n);
+    results.add(BigInt(Math.pow(10, length)) + 1n);
+
+    let difference = BigInt(Number.MAX_SAFE_INTEGER);
+    let result = 0;
+    let actualNumber = BigInt(n);
+
+    for(let num of results){
+        if(num === actualNumber){
+            continue;
+        }
+
+        let currentDifference = num > actualNumber ? num - actualNumber : actualNumber - num;
+
+        if(currentDifference < difference){
+            difference = currentDifference;
+            result = num;
+        }
+        else if(currentDifference === difference){
+            result = num < result ? num : result;
+        }
+    }
+
+    return result.toString();
+
+    function makePalindrome(halfOfN, isEven) {
+        let halfStr = halfOfN.toString();
+        let reversedHalf = halfStr.split('').reverse().join('');
+
+        return BigInt(halfStr + (isEven ? reversedHalf : reversedHalf.slice(1)));
+    }
+};
