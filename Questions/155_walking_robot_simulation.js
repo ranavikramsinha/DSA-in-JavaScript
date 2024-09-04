0fcc646d4be752f6c0f46b2f1d6485e32023a807
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-09-04 22:10:22 +0530

    walking robot simulation problem

---
+++ Questions/155_walking_robot_simulation.js
@@ -0,0 +1,44 @@
+//* https://leetcode.com/problems/walking-robot-simulation/
+
+var robotSim = function(commands, obstacles) {
+    let set = new Set();
+
+    for(let obstacle of obstacles){
+        let initialDirection = `${obstacle[0]}_${obstacle[1]}`;
+        set.add(initialDirection);
+    }
+
+    let x = 0;
+    let y = 0;
+    let maxDistance = 0;
+
+    let direction = [0, 1];
+
+    for(let i = 0; i < commands.length; i++){
+        if(commands[i] === -2){
+            direction = [-direction[1], direction[0]];
+        }
+        else if(commands[i] === -1){
+            direction = [direction[1], -direction[0]];
+        }
+        else{
+            for(let j = 0; j < commands[i]; j++){
+                let newX = x + direction[0];
+                let newY = y + direction[1];
+
+                let nextInitialDirection = `${newX}_${newY}`;
+
+                if(set.has(nextInitialDirection)){
+                    break;
+                }
+
+                x = newX;
+                y = newY;
+            }
+        }
+
+        maxDistance = Math.max(maxDistance, (x * x) + (y * y));
+    }
+
+    return maxDistance;
+};
