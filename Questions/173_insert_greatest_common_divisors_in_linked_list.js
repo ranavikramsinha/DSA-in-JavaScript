fb7e479e0396e0bd79130398476f9d464574366e
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-09-10 21:25:21 +0530

    insert greatest common divisors in linked list problem

--- Questions/173_insert_greatest_common_divisors_in_linked_list.js
+++ Questions/173_insert_greatest_common_divisors_in_linked_list.js
@@ -1,30 +1,88 @@
 //* https://leetcode.com/problems/insert-greatest-common-divisors-in-linked-list/
 
+//* Iterative Approach
+// var insertGreatestCommonDivisors = function(head) {
+//     if(!head === null || !head.next === null){
+//         return head;
+//     }
+
+//     function gcd(a, b){
+//         while(b !== 0){
+//             a = b;
+//             b = a % b;
+//         }
+
+//         return a;
+//     }
+
+//     let currentNode = head;
+
+//     while(currentNode.next){
+//         let nextCurrentNode = currentNode.next;
+//         let value = gcd(currentNode.val, nextCurrentNode.val);
+//         let valueNode = new ListNode(value);
+//         currentNode.next = valueNode;
+//         valueNode.next = nextCurrentNode;
+//         currentNode = nextCurrentNode;
+//         nextCurrentNode = nextCurrentNode.next;
+//     }
+
+//     return head;
+// };
+
 var insertGreatestCommonDivisors = function(head) {
-    if(!head === null || !head.next === null){
+    if(head === null || head.next === null){
         return head;
     }
 
     function gcd(a, b){
         while(b !== 0){
-            a = b;
+            let temp = b;
             b = a % b;
+            a = temp;
         }
 
         return a;
     }
 
     let currentNode = head;
+    let nextCurrentNode = currentNode.next;
 
     while(currentNode.next){
-        let nextCurrentNode = currentNode.next;
         let value = gcd(currentNode.val, nextCurrentNode.val);
         let valueNode = new ListNode(value);
+        
         currentNode.next = valueNode;
         valueNode.next = nextCurrentNode;
         currentNode = nextCurrentNode;
         nextCurrentNode = nextCurrentNode.next;
     }
+
+    return head;
+};
+
+//* Recursive Approach
+var insertGreatestCommonDivisors = function(head) {
+    if(head === null || head.next === null){
+        return head;
+    }
+
+    function gcd(a, b){
+        while(b !== 0){
+            let temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        return a;
+    }
+
+    let temp = insertGreatestCommonDivisors(head.next);
+    let value = gcd(head.val, head.next.val);
+    let valueNode = new ListNode(value);
+
+    valueNode.next = temp;
+    head.next = valueNode;
 
     return head;
 };
