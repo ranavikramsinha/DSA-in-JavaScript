4e2b72a1188a10d8f03730c1d941e2f516f2faf2
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-09-10 22:07:10 +0530

    insert greatest common divisors in linked list problem

--- Questions/173_insert_greatest_common_divisors_in_linked_list.js
+++ Questions/173_insert_greatest_common_divisors_in_linked_list.js
@@ -24,7 +24,6 @@
 //         currentNode.next = valueNode;
 //         valueNode.next = nextCurrentNode;
 //         currentNode = nextCurrentNode;
-//         nextCurrentNode = nextCurrentNode.next;
 //     }
 
 //     return head;
@@ -62,27 +61,27 @@ var insertGreatestCommonDivisors = function(head) {
 };
 
 //* Recursive Approach
-var insertGreatestCommonDivisors = function(head) {
-    if(head === null || head.next === null){
-        return head;
-    }
+// var insertGreatestCommonDivisors = function(head) {
+//     if(head === null || head.next === null){
+//         return head;
+//     }
 
-    function gcd(a, b){
-        while(b !== 0){
-            let temp = b;
-            b = a % b;
-            a = temp;
-        }
+//     function gcd(a, b){
+//         while(b !== 0){
+//             let temp = b;
+//             b = a % b;
+//             a = temp;
+//         }
 
-        return a;
-    }
+//         return a;
+//     }
 
-    let temp = insertGreatestCommonDivisors(head.next);
-    let value = gcd(head.val, head.next.val);
-    let valueNode = new ListNode(value);
+//     let temp = insertGreatestCommonDivisors(head.next);
+//     let value = gcd(head.val, head.next.val);
+//     let valueNode = new ListNode(value);
 
-    valueNode.next = temp;
-    head.next = valueNode;
+//     valueNode.next = temp;
+//     head.next = valueNode;
 
-    return head;
-};
+//     return head;
+// };
