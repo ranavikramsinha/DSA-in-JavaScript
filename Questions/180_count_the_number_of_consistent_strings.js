86a25f01d49a7027d0564e8e42071e28228dfaed
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-09-12 21:24:29 +0530

    count the number of consistent strings problem

--- Questions/180_count_the_number_of_consistent_strings.js
+++ Questions/180_count_the_number_of_consistent_strings.js
@@ -2,10 +2,10 @@
 
 var countConsistentStrings = function(allowed, words) {
 
-    let bit = 0;
+    let bitMask = 0;
 
     for(let char of allowed){
-        bit |= 1 << (char.charCodeAt(0) - 'a'.charCodeAt(0));
+        bitMask |= 1 << (char.charCodeAt(0) - 'a'.charCodeAt(0));
     }
 
     let count = 0;
@@ -14,7 +14,7 @@ var countConsistentStrings = function(allowed, words) {
         let isAllCharactesPresent = true;
 
         for(let i = 0; i < word.length; i++){
-            if(((bit >> (word.charCodeAt(i) - 'a'.charCodeAt(0))) & 1) === 0){
+            if(((bitMask >> (word.charCodeAt(i) - 'a'.charCodeAt(0))) & 1) === 0){
                 isAllCharactesPresent = false;
                 break;
             }
