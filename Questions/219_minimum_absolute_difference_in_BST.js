00df6372f5656b6a0123b806967014d241a2d3a2
Rana Vikram Sinha <168851099+ranavikramsinha@users.noreply.github.com>
2024-09-27 21:23:28 +0530

    minimum absolute difference in BST problem

--- Questions/219_minimum_absolute_difference_in_BST.js
+++ Questions/219_minimum_absolute_difference_in_BST.js
@@ -1,27 +1,28 @@
 //* https://leetcode.com/problems/minimum-absolute-difference-in-bst/
 
-var rangeSumBST = function(root, low, high) {
-    if(root === null){
-        return 0;
+var getMinimumDifference = function(root) {
+    let arr = [];
+
+    function inorder(node){
+        if(node === null){
+            return;
+        }
+
+        inorder(node.left);
+        arr.push(node.val);
+        inorder(node.right);
     }
 
-    if(root.val < low){
-        return rangeSumBST(root.right, low, high);
+    inorder(root);
+
+    let minimum = Infinity;
+
+    for(let i = 1; i < arr.length; i++){
+        let current = arr[i];
+        let previous = arr[i - 1];
+
+        minimum = Math.min(minimum, current - previous);
     }
 
-    if(root.val > high){
-        return rangeSumBST(root.left, low, high);
-    }
-
-    let currentValue = 0;
-
-    if(root.val >= low && root.val <= high){
-        currentValue = root.val;
-    }
-
-    let leftSum = rangeSumBST(root.left, low, high);
-    let rightSum = rangeSumBST(root.right, low, high);
-
-    let total = currentValue + leftSum + rightSum;
-    return total;
+    return minimum;
 };
