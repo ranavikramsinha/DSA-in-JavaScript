7c66202f28ec388005f214a359ea74d8dfc31cfe
Rana Vikram Sinha <168851099+ranavikramsinha@users.noreply.github.com>
2024-10-15 19:05:48 +0530

    linked list cycle problem

--- Questions/247_linked_list_cycle.js
+++ Questions/247_linked_list_cycle.js
@@ -22,3 +22,25 @@ var hasCycle = function(head) {
     return false;
     
 };
+
+//* tc O(n + k) | sc O(1)
+var hasCycle = function(head) {
+
+    if(!head || !head.next){
+        return false;
+    }
+
+    let slow = head;
+    let fast = head.next;
+
+    while(fast !== null && fast.next !== null){
+        slow = slow.next;
+        fast = fast.next.next;
+
+        if(slow === fast){
+            return true;
+        }
+    }
+
+    return false;
+}
