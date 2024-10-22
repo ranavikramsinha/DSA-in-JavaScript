47866b9407a9040bf023a6a39f6fffe94672fcd5
Rana Vikram Sinha <168851099+ranavikramsinha@users.noreply.github.com>
2024-10-22 20:02:14 +0530

    kth largest sum in a binary tree problem

--- Questions/255_kth_largest_sum_in_a_binary_tree.js
+++ Questions/255_kth_largest_sum_in_a_binary_tree.js
@@ -1,5 +1,6 @@
 //* https://leetcode.com/problems/kth-largest-sum-in-a-binary-tree/
 
+//* tc O(n) | sc O(h)
 var kthLargestLevelSum = function(root, k) {
 
     let arr = [];
@@ -29,4 +30,44 @@ var kthLargestLevelSum = function(root, k) {
         solve(node.right, level + 1);
     }
 
+};
+
+//* tc O(nlog(n)) | sc O(n)
+var kthLargestLevelSum = function(root, k) {
+
+    let queue = [root];
+    let minHeap = new MinPriorityQueue({ priority: x => x });
+
+    while(queue.length > 0){
+        let sum = 0;
+        let size = queue.length;
+
+        for(let i = 0; i < size; i++){
+            let current = queue.shift();
+            sum += current.val;
+
+            if(current.left !== null) {
+                queue.push(current.left);
+            }
+
+            if(current.right !== null){
+                queue.push(current.right);
+            }
+        }
+
+        if(minHeap.size() < k){
+            minHeap.enqueue(sum);
+        }
+        else if(minHeap.front().element < sum){
+            minHeap.dequeue();
+            minHeap.enqueue(sum);
+        }
+    }
+
+    if(minHeap.size() < k){
+        return -1;
+    }
+
+    return minHeap.front().element;
+    
 };
