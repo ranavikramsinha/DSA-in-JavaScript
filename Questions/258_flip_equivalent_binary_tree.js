5d624853fd5083634ce88d5ee52bf1bfd49a3ac3
Rana Vikram Sinha <168851099+ranavikramsinha@users.noreply.github.com>
2024-10-24 19:26:55 +0530

    flip equivalent binary tree problem

---
+++ Questions/258_flip_equivalent_binary_tree.js
@@ -0,0 +1,23 @@
+//* https://leetcode.com/problems/flip-equivalent-binary-trees/
+
+var flipEquiv = function(root1, root2) {
+
+    if(root1 === null && root2 === null){
+        return true;
+    }
+
+    if(root1 === null || root2 === null){
+        return false;
+    }
+
+    if(root1.val === root2.val){
+        let notFlipping = flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right);
+        let flipping = flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left);
+
+        return notFlipping || flipping;
+    }
+    else{
+        return false;
+    }
+    
+};
