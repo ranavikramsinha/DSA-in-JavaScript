69fc8119ebf6da6ac57507159a90d848e995da43
Rana Vikram Sinha <168851099+ranavikramsinha@users.noreply.github.com>
2024-10-25 18:33:27 +0530

    remove sub folders from the filesystem problem

--- Questions/259_remove_sub_folders_from_the_filesystem.js
+++ Questions/259_remove_sub_folders_from_the_filesystem.js
@@ -1,5 +1,27 @@
 //* https://leetcode.com/problems/remove-sub-folders-from-the-filesystem/
 
+//* tc O(n * L * logN) | sc O(1)
+var removeSubfolders = function(folder) {
+
+    folder.sort();
+
+    let result = [];
+
+    result.push(folder[0]);
+
+    for(let i = 1; i < folder.length; i++){
+        let currentFolder = folder[i];
+        let lastFolderWithSlash = result[result.length - 1] + "/";
+
+        if(!currentFolder.startsWith(lastFolderWithSlash)){
+            result.push(currentFolder);
+        }
+    }
+
+    return result;
+    
+};
+
 //* tc O(n * L) | sc O(n) where L is the length of the longest path
 var removeSubfolders = function(folder) {
 
