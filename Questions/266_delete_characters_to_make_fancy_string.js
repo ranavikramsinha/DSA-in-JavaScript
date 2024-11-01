9de4042c6cc113e3aa6556e53636af8437b1fd23
Rana Vikram Sinha <168851099+ranavikramsinha@users.noreply.github.com>
2024-11-01 14:50:25 +0530

    delete characters to make fancy string problem

---
+++ Questions/266_delete_characters_to_make_fancy_string.js
@@ -0,0 +1,27 @@
+//* https://leetcode.com/problems/delete-characters-to-make-fancy-string/
+
+//* tc O(n) | sc O(n)
+var makeFancyString = function(s) {
+
+    let result = "";
+    let previous = "";
+    let count = 0;
+
+    for(let currentChar of s){
+        if(previous === currentChar){
+            count++;
+        }
+        else{
+            count = 0;
+        }
+
+        if(count < 2){
+            result += currentChar;
+        }
+
+        previous = currentChar;
+    }
+    
+    return result;
+    
+};
