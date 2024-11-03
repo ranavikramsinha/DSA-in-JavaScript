39fb6572d23c2d7cb1ad4f3a1afe014ce1b007b0
Rana Vikram Sinha <168851099+ranavikramsinha@users.noreply.github.com>
2024-11-03 13:04:20 +0530

    rotate string problem

---
+++ Questions/268_rotate_string.js
@@ -0,0 +1,32 @@
+//* https://leetcode.com/problems/rotate-string/
+
+//* tc O(n^2) | sc O(n)
+var rotateString = function(s, goal) {
+
+    let m = s.length;
+    let n = goal.length;
+
+    for(let i = 0; i < m; i++){
+
+        s = s.slice(1) + s[0];
+
+        if(s === goal){
+            return true;s
+        }
+
+    }
+
+    return false;
+    
+};
+
+//* tc O(n) | sc O(2 * n) which equivalent to O(n)
+var rotateString = function(s, goal) {
+
+    if(s.length !== goal.length){
+        return false;
+    }
+
+    return (s + s).includes(goal);
+    
+};
