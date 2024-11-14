9d8aa8d9d0c06c355c141b4ee1f9c0be3c8dbba9
Rana Vikram Sinha <168851099+ranavikramsinha@users.noreply.github.com>
2024-11-14 21:30:52 +0530

    minimzed maximum of products distributed to any store problem

--- Questions/281_minimized_maximum_of_products_distributed_to_any_store.js
+++ Questions/281_minimized_maximum_of_products_distributed_to_any_store.js
@@ -33,3 +33,35 @@ var minimizedMaximum = function(n, quantities) {
     }
     
 };
+
+//* same as above but different logic that's it
+var minimizedMaximum = function(n, quantities) {
+    let left = 1;
+    let right = Math.max(...quantities);
+    let ans = 0;
+
+    while(left <= right){
+        let middle = left + Math.trunc((right - left) / 2);
+
+        if(solve(middle, quantities, n)){
+            ans = middle;
+            right = middle - 1;
+        }
+        else{
+            left = middle + 1;
+        }
+    }
+
+    return ans;
+
+    function solve(x, quantities, stores){
+        let num = 0;
+
+        for(let q of quantities){
+            num += Math.ceil(q / x);
+        }
+
+        return num <= stores;
+    }
+    
+};
