99763287a2504da777b404c3ebf985235a3ab772
Rana Vikram Sinha <168851099+ranavikramsinha@users.noreply.github.com>
2024-11-15 20:24:16 +0530

    shortest subarray to be removed to make array sorted problem

---
+++ Questions/282_shortest_subarray_to_be_removed_to_make_array_sorted.js
@@ -0,0 +1,37 @@
+//* https://leetcode.com/problems/shortest-subarray-to-be-removed-to-make-array-sorted/
+
+//* tc O(n) | sc O(1)
+var findLengthOfShortestSubarray = function(arr) {
+
+    let n = arr.length;
+    let left = 0;
+    let right = n - 1;
+
+    while(left + 1 < n && arr[left] <= arr[left + 1]){
+        left++;
+    }
+
+    while(right - 1 >= 0 && arr[right] >= arr[right - 1]){
+        right--;
+    }
+
+    if(left >= right){
+        return 0;
+    }
+    
+    let ans = right;
+    let i = 0;
+    let j = right;
+
+    while(i <= left){
+        while(j < n && arr[i] > arr[j]){
+            j++;
+        }
+
+        ans = Math.min(ans, j - i - 1);
+
+        i++;
+    }
+
+    return ans;
+};
