eaf6cac15c7e8d876c0feaefd9553ac451c7b825
Rana Vikram Sinha <168851099+ranavikramsinha@users.noreply.github.com>
2024-11-16 07:39:19 +0530

    find the power of k size subarrays i problem

---
+++ Questions/283_find_the_power_of_k_size_subarrays_i.js
@@ -0,0 +1,29 @@
+//* https://leetcode.com/problems/find-the-power-of-k-size-subarrays-i/
+
+//* tc O(n) | sc O(n)
+var resultsArray = function(nums, k) {
+    let n = nums.length;
+    let increasingOrderSubarraySize = 1;
+    let result = [];
+
+    for(let i = 0; i < n; i++){
+
+        if(i > 0 && nums[i] === nums[i - 1] + 1){
+            increasingOrderSubarraySize += 1;
+        }
+        else{
+            increasingOrderSubarraySize = 1;
+        }
+
+        if(i >= k - 1){
+            if(increasingOrderSubarraySize >= k){
+                result.push(nums[i]);
+            }
+            else{
+                result.push(-1);
+            }
+        }
+    }
+
+    return result;
+};
