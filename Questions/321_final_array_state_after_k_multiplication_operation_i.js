e0b64c8e391effaa3caa8411729fd48f9728f4d8
Rana Vikram Sinha <168851099+ranavikramsinha@users.noreply.github.com>
2024-12-16 23:35:26 +0530

    final array state after k multiplication operation i problme

---
+++ Questions/321_final_array_state_after_k_multiplication_operation_i.js
@@ -0,0 +1,35 @@
+//* https://leetcode.com/problems/final-array-state-after-k-multiplication-operations-i/
+
+//* tc O(nlogn + klogn) | sc O(n)
+
+var getFinalState = function(nums, k, multiplier) {
+    let n = nums.length;
+    let arr = [];
+
+    for(let i = 0; i < n; i++){
+        arr.push([nums[i], i]);
+    }
+
+    let compare = (a, b) => {
+        if(a[0] === b[0]){
+            return a[1] - b[1];
+        }
+
+        return a[0] - b[0];
+    }
+
+    arr.sort(compare);
+    
+    while(k > 0){
+        let smallest = arr.shift();
+
+        nums[smallest[1]] *= multiplier;
+
+        arr.push([nums[smallest[1]], smallest[1]]);
+        arr.sort(compare);
+
+        k--;
+    }
+
+    return nums;
+};
