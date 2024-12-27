70a1e31b2f9b04439c582475874467fefe6e9100
Rana Vikram Sinha <168851099+ranavikramsinha@users.noreply.github.com>
2024-12-27 22:36:12 +0530

    best sightseeing pair problem

--- Questions/341_best_sightseeing_pair.js
+++ Questions/341_best_sightseeing_pair.js
@@ -21,3 +21,26 @@ var maxScoreSightseeingPair = function(values) {
     return ans;
     
 };
+
+//* tc O(n) | sc O(n)
+
+var maxScoreSightseeingPair = function(values) {
+
+    let n = values.length;
+    let arr = new Array(n).fill(0);
+    let ans = 0;
+
+    arr[0] = values[0];
+
+    for(let j = 1; j < n; j++){
+        let a = values[j] - j;
+        let b = arr[j - 1];
+
+        ans = Math.max(ans, a + b);
+
+        arr[j] = Math.max(values[j] + j, arr[j -1]);
+    }
+
+    return ans;
+    
+};
