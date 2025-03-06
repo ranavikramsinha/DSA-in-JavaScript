12df4689891fae23a012c8165b7bd37fea8a9638
Rana Vikram Sinha <168851099+ranavikramsinha@users.noreply.github.com>
2025-03-06 21:33:58 +0530

    find missing and repeated values problem (multiple solutions)

--- Questions/427_find_missing_and_repeated_values.js
+++ Questions/427_find_missing_and_repeated_values.js
@@ -28,3 +28,36 @@ var findMissingAndRepeatedValues = function(grid) {
     return [a, b];
     
 };
+
+//* tc O(n^2) | sc O(n^2) 
+
+var findMissingAndRepeatedValues = function(grid) {
+
+    let n = grid.length;
+    let num = n * n;
+    let map = new Map();
+    let a = -1;
+    let b = -1;
+
+    for(let i = 0; i < n; i++){
+        for(let j = 0; j < n; j++){
+            map.set(grid[i][j], (map.get(grid[i][j]) || 0) + 1);
+        }
+    }
+
+    for(let i = 1; i <= num; i++){
+        if(map.get(i) === 2){
+            a = i;
+        }
+        else if(!map.has(i)){
+            b = i;
+        }
+
+        if(a !== -1 && b !== -1){
+            break;
+        }
+    }
+
+    return [a, b];
+    
+};
