4d2033a232ec4e19beb56b995538931c06004726
Rana Vikram Sinha <168851099+ranavikramsinha@users.noreply.github.com>
2025-03-18 21:45:45 +0530

    longest nice subarray problem ( O(n) and O(n * n) )

--- Questions/439_longest_nice_subarray.js
+++ Questions/439_longest_nice_subarray.js
@@ -1,5 +1,30 @@
 //* https://leetcode.com/problems/longest-nice-subarray/
 
+//* tc O(n) | sc O(1)
+
+var longestNiceSubarray = function(nums) {
+
+    let n = nums.length;
+    let result = 1;
+    let i = 0;
+    let j = 0;
+    let mask = 0;
+
+    while(j < n){
+        while((mask & nums[j]) !== 0){
+            mask ^= nums[i];
+            i++;
+        }
+
+        result = Math.max(result, j - i + 1);
+        mask |= nums[j];
+        j++;
+    }
+
+    return result;
+    
+};
+
 //* tc O(n * n) | sc O(1)
 
 var longestNiceSubarray = function(nums) {
