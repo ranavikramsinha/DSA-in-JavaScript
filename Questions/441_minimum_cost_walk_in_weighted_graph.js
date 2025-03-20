82cec51028a0b06c6c45d6cb65f7b1195c02d72c
Rana Vikram Sinha <168851099+ranavikramsinha@users.noreply.github.com>
2025-03-20 22:55:12 +0530

    minimum cost walk in weighted graph problem

---
+++ Questions/441_minimum_cost_walk_in_weighted_graph.js
@@ -0,0 +1,56 @@
+//* https://leetcode.com/problems/minimum-cost-walk-in-weighted-graph/
+
+//* tc O(n + e + q) | sc O(n)
+
+var minimumCost = function(n, edges, query) {
+
+    let roots = new Array(n);
+    let componentCosts = new Array(n);
+
+    for(let node = 0; node < n; node++){
+        roots[node] = node;
+        componentCosts[node] = -1;
+    }
+
+    for(let [u, v, w] of edges){
+        let rootSource = findingRoot(u);
+        let rootDestination = findingRoot(v);
+
+        if(rootSource !== rootDestination){
+            componentCosts[rootSource] = componentCosts[rootSource] & componentCosts[rootDestination];
+            roots[rootDestination] = rootSource;
+        }
+
+        componentCosts[rootSource] = componentCosts[rootSource] & w;
+    }
+
+    let result = [];
+
+    for(let [startNode, endNode] of query){
+        if(startNode === endNode){
+            result.push(0);
+            continue;
+        }
+
+        let rootStart = findingRoot(startNode);
+        let rootEnd = findingRoot(endNode);
+
+        if(rootStart !== rootEnd){
+            result.push(-1);
+        }
+        else{
+            result.push(componentCosts[rootStart]);
+        }
+    }
+
+    return result;
+
+    function findingRoot(node){
+        if(roots[node] !== node){
+            roots[node] = findingRoot(roots[node]);
+        }
+
+        return roots[node];
+    }
+    
+};
