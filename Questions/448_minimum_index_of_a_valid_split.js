3870f8ddff7825f166e7abfdb93b636e7e02fe00
Rana Vikram Sinha <168851099+ranavikramsinha@users.noreply.github.com>
2025-03-27 19:37:58 +0530

    minimum index of a valid split problem ( in sc O(1) used boyer moore algo )

--- Questions/448_minimum_index_of_a_valid_split.js
+++ Questions/448_minimum_index_of_a_valid_split.js
@@ -1,5 +1,54 @@
 //* https://leetcode.com/problems/minimum-index-of-a-valid-split/
 
+//* tc O(n) | sc O(1)
+
+var minimumIndex = function(nums) {
+
+    let n = nums.length;
+    let majorityElement = -1;
+    let majorityElementCount = 0;
+    let count = 0;
+
+    for(let i = 0; i < n; i++){
+        if(count === 0){
+            majorityElement = nums[i];
+            count++;
+        }
+        else if(nums[i] === majorityElement){
+            count++;
+        }
+        else{
+            count--;
+        }
+    }
+
+    for(let num of nums){
+        if(num === majorityElement){
+            majorityElementCount++;
+        }
+    }
+
+    count = 0;
+
+    for(let i = 0; i < n; i++){
+        if(nums[i] === majorityElement){
+            count++;
+        }
+
+        let remainingCount = majorityElementCount - count;
+
+        let n1 = i + 1;
+        let n2 = n - i - 1;
+
+        if(count > Math.trunc(n1 / 2) && remainingCount > Math.trunc(n2 / 2)){
+            return i;
+        }
+    }
+
+    return -1;
+    
+};
+
 //* tc O(n) | sc O(n)
 
 var minimumIndex = function(nums) {
