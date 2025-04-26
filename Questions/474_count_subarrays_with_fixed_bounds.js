ca8eab1c4ed7207a392e4ebb06e0f40b6d3900e5
Rana Vikram Sinha <168851099+ranavikramsinha@users.noreply.github.com>
2025-04-26 22:34:06 +0530

    count subarray with fixed bounds problem

---
+++ Questions/474_count_subarrays_with_fixed_bounds.js
@@ -0,0 +1,34 @@
+//* https://leetcode.com/problems/count-subarrays-with-fixed-bounds/
+
+//* tc O(n) | sc O(1)
+
+var countSubarrays = function(nums, minK, maxK) {
+
+    let n = nums.length;
+    let minKValuePosition = -1;
+    let maxKValuePosition = -1;
+    let valueGreaterAndLessThanMaxAndMinPosition = -1;
+    let result = 0;
+
+    for(let i = 0; i < n; i++){
+        if(nums[i] < minK || nums[i] > maxK){
+            valueGreaterAndLessThanMaxAndMinPosition = i;
+        }
+
+        if(nums[i] === minK){
+            minKValuePosition = i;
+        }
+
+        if(nums[i] === maxK){
+            maxKValuePosition = i;
+        }
+
+        let minimum = Math.min(minKValuePosition, maxKValuePosition);
+        let validSubArray = minimum - valueGreaterAndLessThanMaxAndMinPosition;
+
+        result += (validSubArray <= 0) ? 0 : validSubArray;
+    }
+
+    return result;
+    
+};
