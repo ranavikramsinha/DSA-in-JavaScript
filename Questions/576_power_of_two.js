d5da5d3e599242f8c33a2423e5bf0ce855a4488f
Rana Vikram Sinha <168851099+ranavikramsinha@users.noreply.github.com>
2025-08-09 22:26:24 +0530

    power of two problem

---
+++ Questions/576_power_of_two.js
@@ -0,0 +1,16 @@
+//* https://leetcode.com/problems/power-of-two/
+
+//* tc : O(log(n)) | sc : O(log(n))
+
+var isPowerOfTwo = function(n) {
+
+    if(n <= 0){
+        return false;
+    }
+    else if(n === 1){
+        return true;
+    }
+
+    return n % 2 === 0 && isPowerOfTwo(n / 2);
+    
+};
