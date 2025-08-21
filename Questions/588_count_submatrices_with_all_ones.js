4c38adc7b246444da9834a7eb25573a598ca8600
Rana Vikram Sinha <168851099+ranavikramsinha@users.noreply.github.com>
2025-08-21 21:34:10 +0530

    count submatrices with all ones problem

---
+++ Questions/588_count_submatrices_with_all_ones.js
@@ -0,0 +1,43 @@
+//* https://leetcode.com/problems/count-submatrices-with-all-ones/
+
+//* tc : O(row ^ 2 * col) | sc : O(row * col)
+
+var numSubmat = function(mat) {
+
+    let m = mat.length;
+    let n = mat[0].length;
+    let result = 0;
+
+    for(let startRow = 0; startRow < m; startRow++){
+        let colTracking = new Array(n).fill(1);
+
+        for(let endRow = startRow; endRow < m; endRow++){
+            for(let col = 0; col < n; col++){
+                colTracking[col] = colTracking[col] & mat[endRow][col];
+            }
+
+            result += solve(colTracking);
+        }
+    }
+
+    return result;
+
+    function solve(arr){
+        let streak = 0;
+        let total = 0;
+
+        for(let num of arr){
+            if(num === 0){
+                streak = 0;
+            }
+            else{
+                streak++;
+            }
+
+            total += streak;
+        }
+
+        return total;
+    }
+    
+};
