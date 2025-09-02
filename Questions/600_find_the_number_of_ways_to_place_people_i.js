316851750d8f627b220a8c7fa19d1b3949fe3546
Rana Vikram Sinha <168851099+ranavikramsinha@users.noreply.github.com>
2025-09-02 21:18:30 +0530

    find the number of ways to place people i problem

---
+++ Questions/600_find_the_number_of_ways_to_place_people_i.js
@@ -0,0 +1,38 @@
+//* https://leetcode.com/problems/find-the-number-of-ways-to-place-people-i/
+
+//* tc : O(n^2) | sc : O(1)
+
+var numberOfPairs = function(points) {
+
+    let n = points.length;
+
+    points.sort((a, b) => {
+        if (a[0] === b[0]) {
+            return b[1] - a[1];
+        }
+        return a[0] - b[0];
+    });
+
+    let result = 0;
+
+    for (let i = 0; i < n; i++) {
+        let [x1, y1] = points[i];
+        let bestY = -Infinity;
+
+        for (let j = i + 1; j < n; j++) {
+            let [x2, y2] = points[j];
+
+            if (y2 > y1) {
+                continue;
+            }
+
+            if (y2 > bestY) {
+                result++;
+                bestY = y2;
+            }
+        }
+    }
+
+    return result;
+    
+};
