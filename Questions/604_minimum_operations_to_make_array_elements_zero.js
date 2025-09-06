56a405b52a635c9d142f2e1eb656bf10f0fd8c79
Rana Vikram Sinha <168851099+ranavikramsinha@users.noreply.github.com>
2025-09-06 22:38:28 +0530

    minimum operations to make array elements zero problem

---
+++ Questions/604_minimum_operations_to_make_array_elements_zero.js
@@ -0,0 +1,41 @@
+//* https://leetcode.com/problems/minimum-operations-to-make-array-elements-zero/
+
+//* tc : O(m * log(r)) | sc : O(1)
+
+var minOperations = function(queries) {
+
+    let totalOperations = 0n;
+
+    for (const [left, right] of queries) {
+        let steps = getSteps(left, right);
+        totalOperations += (steps + 1n) / 2n;
+    }
+
+    return totalOperations <= BigInt(Number.MAX_SAFE_INTEGER) ? Number(totalOperations) : totalOperations.toString();
+
+    function getSteps(left, right) {
+        let blockStart = 1n;
+        let weight = 1n;
+        let totalSteps = 0n;
+
+        const L = BigInt(left);
+        const R = BigInt(right);
+
+        while (blockStart <= R) {
+        let blockEnd = 4n * blockStart - 1n;
+        let overlapStart = L > blockStart ? L : blockStart;
+        let overlapEnd = R < blockEnd ? R : blockEnd;
+
+        if (overlapStart <= overlapEnd) {
+            let overlapCount = overlapEnd - overlapStart + 1n;
+            totalSteps += overlapCount * weight;
+        }
+
+        weight += 1n;
+        blockStart *= 4n;
+        }
+
+        return totalSteps;
+    }
+    
+};
