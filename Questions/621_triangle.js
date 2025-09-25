967e699d26de4d8e02495e6c08fd7d278b9ac2b2
Rana Vikram Sinha <168851099+ranavikramsinha@users.noreply.github.com>
2025-09-25 20:35:53 +0530

    triangle problem

---
+++ Questions/621_triangle.js
@@ -0,0 +1,18 @@
+//* https://leetcode.com/problems/triangle/
+
+//* tc : O(n^2) | sc : O(n)
+
+var minimumTotal = function(triangle) {
+
+    let n = triangle.length;
+    let dp = triangle[n - 1].slice();
+
+    for (let i = n - 2; i >= 0; i--) {
+        for (let j = 0; j <= i; j++) {
+            dp[j] = triangle[i][j] + Math.min(dp[j], dp[j + 1]);
+        }
+    }
+    
+    return dp[0];
+    
+};
