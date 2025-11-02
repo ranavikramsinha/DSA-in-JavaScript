bf23fa340d74c613b7ab1b8983fbacea573f9139
Rana Vikram Sinha <168851099+ranavikramsinha@users.noreply.github.com>
2025-11-02 22:00:22 +0530

    merge two sorted lists problem

---
+++ Questions/652_merge_two_sorted_lists.js
@@ -0,0 +1,27 @@
+//* https://leetcode.com/problems/merge-two-sorted-lists/
+
+//* tc : O(m + n) | sc : O(1)
+
+var mergeTwoLists = function(list1, list2) {
+
+    let tempNode = new ListNode(0, null);
+    let currentNode = tempNode;
+    
+    while (list1 && list2) {
+        if(list1.val < list2.val) {
+            currentNode.next = list1;
+            list1 = list1.next
+        }
+        else {
+            currentNode.next = list2;
+            list2 = list2.next
+        }
+
+        currentNode = currentNode.next;
+    }
+
+    currentNode.next = list1 || list2;
+    
+    return tempNode.next;
+    
+};
